53660dfac48d2600049f473b191adc70ca177b2a
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-04-25 19:37:31 +1000

    init

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,4 +1,3 @@
-/* eslint-disable */ 
 // Nextresponce is an extension of the native response interface
 import { NextResponse } from 'next/server';
 import { verifyToken } from '../lib/utils';
@@ -11,26 +10,14 @@ export default async function middleware(req) {
   const userId = await verifyToken(token);
  
   const nextUrl = req.nextUrl.clone();
-  const { pathname } = req.nextUrl;
 
-  if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
+  if(nextUrl.pathname.includes('/api/login') || userId || nextUrl.pathname.includes('/static')) {
     return NextResponse.next();
   };
     
   // If no token return to login
-  if (!token && pathname !== '/login') {
+  if (!token && nextUrl.pathname !== '/login') {
       nextUrl.pathname = '/login';
       return NextResponse.redirect(nextUrl);
   };
 };
-
-
-// const { pathname } = req.nextUrl;
-
-  // if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
-  //   return NextResponse.next();
-  // }
-
-  // if (!token && pathname !== '/login') {
-  //   return NextResponse.redirect('/login');
-  // }
