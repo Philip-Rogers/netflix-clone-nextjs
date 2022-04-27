6356753e844755821a31197bef341dbb92827b24
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-04-27 11:36:49 +1000

    init

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,38 +1,25 @@
 // NextResponse is an extension of the native response interface
-import { NextResponse, } from 'next/server';
+import { NextResponse } from 'next/server';
 import { verifyToken } from '../lib/utils';
 
 
 // Function to check token verification at login level
 // If verification fails api routes will not execute and user will be redirected to login
-// export async function middleware(req) {
-//   const token = req ? req.cookies?.token : null;
-//   const userId = await verifyToken(token);
- 
-//   const url = req.nextUrl.clone();
-//   const { pathname } = req.nextUrl;
-
-//   // Skip static folder path so images load onto page
-//   if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
-//     return NextResponse.next();
-//   }
-    
-//   // If no token return to login
-//   if (!token && pathname !== '/login') {
-//       url.pathname = '/login';
-//       return NextResponse.redirect(url);
-//   }
-// };
-
-export async function middleware(req, event) {
+export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
   const userId = await verifyToken(token);
-  const {pathname, origin} = req.nextUrl.clone(event);
+ 
+  const url = req.nextUrl.clone();
+  const { pathname } = req.nextUrl;
 
-  if ((token && userId) || pathname.includes(`/api/login`) || pathname.includes('/static')) {
-      return NextResponse.next()
+  // Skip static folder path so images load onto page
+  if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
+    return NextResponse.next();
   }
-  if (!token && pathname !== `/login`) {
-      return NextResponse.redirect(`${origin}/login`)
+    
+  // If no token return to login
+  if (!token && pathname !== '/login') {
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
   }
-}
+};
