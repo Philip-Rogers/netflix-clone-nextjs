7b0226308338928b5f387eba6b7fc5550ea2bb50
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-04-27 11:13:42 +1000

    init

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,25 +1,38 @@
 // NextResponse is an extension of the native response interface
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest, NextFetchEvent } from 'next/server';
 import { verifyToken } from '../lib/utils';
 
 
 // Function to check token verification at login level
 // If verification fails api routes will not execute and user will be redirected to login
-export async function middleware(req) {
-  const token = req ? req.cookies?.token : null;
-  const userId = await verifyToken(token);
+// export async function middleware(req) {
+//   const token = req ? req.cookies?.token : null;
+//   const userId = await verifyToken(token);
  
-  const url = req.nextUrl.clone();
-  const { pathname } = req.nextUrl;
+//   const url = req.nextUrl.clone();
+//   const { pathname } = req.nextUrl;
 
-  // Skip static folder path so images load onto page
-  if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
-    return NextResponse.next();
-  }
+//   // Skip static folder path so images load onto page
+//   if(pathname.includes('/api/login') || userId || pathname.includes('/static')) {
+//     return NextResponse.next();
+//   }
     
-  // If no token return to login
-  if (!token && pathname !== '/login') {
-      url.pathname = '/login';
-      return NextResponse.redirect(url);
+//   // If no token return to login
+//   if (!token && pathname !== '/login') {
+//       url.pathname = '/login';
+//       return NextResponse.redirect(url);
+//   }
+// };
+
+export async function middleware(req, NextRequest, NextFetchEvent) {
+  const token = req ? req.cookies?.token : ''
+  const userId = await verifyToken(token)
+  const {pathname, origin} = req.nextUrl.clone()
+
+  if ((token && userId) || pathname.includes(`/api/login`)) {
+      return NextResponse.next()
   }
-};
+  if (!token && pathname !== `/login`) {
+      return NextResponse.redirect(`${origin}/login`)
+  }
+}
